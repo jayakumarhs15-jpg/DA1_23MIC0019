9fd14f07fba355909df3e42aab3917845a8b9508
Jayakumar H S <jayakumarhs15@gmail.com>
2026-07-31 00:00:28 +0530

    Initial commit - Java CI program

---
+++ src/com/jayakumar/StudentCI.java
@@ -0,0 +1,12 @@
+package com.jayakumar;
+
+public class StudentCI {
+
+	public static void main(String[] args) {
+		System.out.println("Student Name : Jayakumar H S");
+		System.out.println("Register Number : 23MIC0019");
+		System.out.println("Message : Jenkins-GitHub Integration Successful");
+
+	}
+
+}
